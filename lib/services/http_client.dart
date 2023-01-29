96a393b52f4b126d7497e6bf4568447cd93ab206
Empylo <admin@empylo.com>
2023-01-29 12:20:41 +0000

    Add update and delete methods to HttpClient.

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -1,7 +1,7 @@
 /*
 An example of an endpoint is:
 ```
-
+select
 curl 'https://banckend.com/rest/v1/users?select=id' \
 -H "apikey: KEY" \
 -H "Authorization: Bearer KEY"
@@ -40,6 +40,130 @@ class HttpClient {
         return await _dio.post(
           url,
           data: data,
+          options: Options(headers: headers),
+        );
+      } on DioError catch (e) {
+        if (e.response != null) {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': e.response!.data},
+          ));
+        } else {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': 'retry'},
+          ));
+        }
+        retries++;
+        await Future.delayed(Duration(milliseconds: delay));
+        delay *= 2;
+      }
+    }
+    return Response(
+        requestOptions: RequestOptions(path: url),
+        statusCode: 500,
+        statusMessage:
+            'Error: Could not connect to server. Retried $retries times.');
+  }
+
+  Future<Response> get({
+    required String url,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+    int maxRetries = 3,
+    int initialDelay = 1000,
+  }) async {
+    int retries = 0;
+    int delay = initialDelay;
+    while (retries < maxRetries) {
+      try {
+        return await _dio.get(
+          url,
+          options: Options(headers: headers),
+          queryParameters: queryParameters,
+        );
+      } on DioError catch (e) {
+        if (e.response != null) {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': e.response!.data},
+          ));
+        } else {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': 'retry'},
+          ));
+        }
+        retries++;
+        await Future.delayed(Duration(milliseconds: delay));
+        delay *= 2;
+      }
+    }
+    return Response(
+        requestOptions: RequestOptions(path: url),
+        statusCode: 500,
+        statusMessage:
+            'Error: Could not connect to server. Retried $retries times.');
+  }
+
+  Future<Response> update({
+    required String url,
+    Map<String, dynamic>? headers,
+    dynamic data,
+    int maxRetries = 3,
+    int initialDelay = 1000,
+  }) async {
+    int retries = 0;
+    int delay = initialDelay;
+    while (retries < maxRetries) {
+      try {
+        return await _dio.put(
+          url,
+          data: data,
+          options: Options(headers: headers),
+        );
+      } on DioError catch (e) {
+        if (e.response != null) {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': e.response!.data},
+          ));
+        } else {
+          await _sentry.sendErrorEvent(ErrorEvent(
+            message: e.toString(),
+            level: 'error',
+            extra: {'context': 'retry'},
+          ));
+        }
+        retries++;
+        await Future.delayed(Duration(milliseconds: delay));
+        delay *= 2;
+      }
+    }
+    return Response(
+        requestOptions: RequestOptions(path: url),
+        statusCode: 500,
+        statusMessage:
+            'Error: Could not connect to server. Retried $retries times.');
+  }
+
+  Future<Response> delete({
+    required String url,
+    Map<String, dynamic>? headers,
+    int maxRetries = 3,
+    int initialDelay = 1000,
+  }) async {
+    int retries = 0;
+    int delay = initialDelay;
+    while (retries < maxRetries) {
+      try {
+        return await _dio.delete(
+          url,
           options: Options(headers: headers),
         );
       } on DioError catch (e) {
--- test/http_client_test.dart
+++ test/http_client_test.dart
@@ -30,7 +30,7 @@ void main() {
     );
   });
 
-  group('HttpClient', () {
+  group('HttpClient Post', () {
     test('should return response', () async {
       // arrange
       when(
@@ -112,6 +112,300 @@ void main() {
 
       // act
       final response = await client.post(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 500);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+
+      verify(
+        () => sentryDio.post(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).called(3);
+    });
+  });
+
+  group('HttpClient Get', () {
+    test('should return response', () async {
+      // arrange
+      when(
+        () => dio.get(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 200,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.get(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 200);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should return error response', () async {
+      // arrange
+      when(
+        () => dio.get(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.get(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 400);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should handle error', () async {
+      // arrange
+      when(
+        () => dio.get(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(
+            path: 'https://banckend.com/rest/v1/users?select=id',
+          ),
+          response: Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.get(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 500);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+
+      verify(
+        () => sentryDio.post(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).called(3);
+    });
+  });
+
+  group('HttpClient Update', () {
+    test('should return response', () async {
+      // arrange
+      when(
+        () => dio.put(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 200,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.update(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 200);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should return error response', () async {
+      // arrange
+      when(
+        () => dio.put(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.update(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 400);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should handle error', () async {
+      // arrange
+      when(
+        () => dio.put(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(
+            path: 'https://banckend.com/rest/v1/users?select=id',
+          ),
+          response: Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.update(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 500);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+        
+      verify(
+        () => sentryDio.post(
+          any(),
+          data: any(named: 'data'),
+          options: any(named: 'options'),
+        ),
+      ).called(3);
+    });
+  });
+
+  group('HttpClient Delete', () {
+    test('should return response', () async {
+      // arrange
+      when(
+        () => dio.delete(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 200,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.delete(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 200);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should return error response', () async {
+      // arrange
+      when(
+        () => dio.delete(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.delete(
+        url: 'https://banckend.com/rest/v1/users?select=id',
+      );
+
+      // assert
+      expect(response.statusCode, 400);
+      expect(response.requestOptions.path,
+          'https://banckend.com/rest/v1/users?select=id');
+    });
+
+    test('should handle error', () async {
+      // arrange
+      when(
+        () => dio.delete(
+          any(),
+          options: any(named: 'options'),
+        ),
+      ).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(
+            path: 'https://banckend.com/rest/v1/users?select=id',
+          ),
+          response: Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(
+                path: 'https://banckend.com/rest/v1/users?select=id'),
+          ),
+        ),
+      );
+
+      // act
+      final response = await client.delete(
         url: 'https://banckend.com/rest/v1/users?select=id',
       );
 
